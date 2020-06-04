ea0670720d40058733f2e13d1e31f88c2559c300
powerboat9 <powerboat9.gamer@gmail.com>
2020-06-04 09:03:52 -0400

    Added subcommands, support for non-1080p resolutions

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use scraper::{Html, Selector};
 use regex::Regex;
 use std::time::{SystemTime, UNIX_EPOCH};
 use std::fmt::{Display, Formatter};
-use clap::{App, Arg};
+use clap::{App, Arg, SubCommand};
 use std::process::exit;
 
 extern crate ureq;
@@ -12,11 +12,47 @@ extern crate clap;
 #[macro_use]
 extern crate lazy_static;
 
-fn main() {
+fn main() -> Result<(), PageError> {
     let a = App::new("Horrible Subs Downloader")
         .version("1.0")
         .author("powerboat9")
         .about("Gets a list of 1080p magnet links for shows on horriblesubs.info")
+        .subcommand(
+            SubCommand::with_name("getmags")
+                .about("Gets a list of magnet links from a show")
+                .arg(
+                    Arg::with_name("SHOW")
+                        .help("the show to download, by default the show's url")
+                        .required(true)
+                        .index(1)
+                )
+                .arg(
+                    Arg::with_name("batch")
+                        .short("b")
+                        .help("shows batches instead of episodes")
+                )
+                .arg(
+                    Arg::with_name("res")
+                        .short("r")
+                        .value_name("RESOLUTION")
+                        .help("sets the resolution to download in: 1080 | 720 | 480 | auto")
+                        .takes_value(true)
+                )
+        )
+        .subcommand(
+            SubCommand::with_name("list")
+                .about("List shows available on HorribleSubs")
+        )
+        .subcommand(
+            SubCommand::with_name("getid")
+                .about("Gets the id associated with a show url")
+                .arg(
+                    Arg::with_name("SHOW")
+                        .help("the show to download, by default the show's url")
+                        .required(true)
+                        .index(1)
+                )
+        )
         .arg(
             Arg::with_name("only-links")
                 .short("l")
@@ -27,32 +63,9 @@ fn main() {
                 .short("i")
                 .help("accept/display show ids instead of urls")
         )
-        .arg(
-            Arg::with_name("batch")
-                .short("b")
-                .help("shows batches instead of episodes")
-        )
-        .arg(
-            Arg::with_name("SHOW")
-                .help("the show to download, by default the show's url, \"list\" to list shows")
-                .required(true)
-                .index(1)
-        )
         .get_matches();
-    let id = a.value_of("SHOW").unwrap();
-    if id == "list" {
-        let list = get_show_list().unwrap();
-        let only_links = a.is_present("only-links");
-        let show_id = a.is_present("use-id");
-        for e in list.iter() {
-            match (only_links, show_id) {
-                (false, false) => println!("{}: {}", e.0, e.1),
-                (false, true) => println!("{}: {}", e.0, get_show_id(e.1.as_str()).unwrap()),
-                (true, false) => println!("{}", e.1),
-                (true, true) => println!("{}", get_show_id(e.1.as_str()).unwrap())
-            }
-        }
-    } else {
+    if let Some(submatch) = a.subcommand_matches("getmags") {
+        let id = submatch.value_of("SHOW").unwrap();
         let id: u32 = if a.is_present("use-id") {
             match id.parse() {
                 Ok(o) => o,
@@ -62,15 +75,16 @@ fn main() {
                 }
             }
         } else {
-            match get_show_id(id) {
-                Ok(v) => v,
-                Err(e) => {
-                    eprintln!("[ERROR]: {}", e);
-                    exit(-2)
-                }
-            }
+            get_show_id(id)?
         };
-        let list = get_show_torrents(id, get_epoch(), a.is_present("batch")).unwrap();
+        let link_type = match submatch.value_of("res") {
+            Some("1080") => LinkType::Res1080,
+            Some("720") => LinkType::Res720,
+            Some("480") => LinkType::Res480,
+            None | Some("auto") => LinkType::Auto,
+            _ => return Err(PageError::CommandLineError("unrecognized resolution"))
+        };
+        let list = get_show_torrents(id, get_epoch(), link_type, a.is_present("batch"))?;
         let only_links = a.is_present("only-links");
         for e in list {
             if only_links {
@@ -80,14 +94,44 @@ fn main() {
                 println!("link: {}", e.1);
             }
         }
+    } else if a.subcommand_matches("list").is_some() {
+        let list = get_show_list()?;
+        let only_links = a.is_present("only-links");
+        let show_id = a.is_present("use-id");
+        for e in list.iter() {
+            match (only_links, show_id) {
+                (false, false) => println!("{}: {}", e.0, e.1),
+                (false, true) => println!("{}: {}", e.0, get_show_id(e.1.as_str())?),
+                (true, false) => println!("{}", e.1),
+                (true, true) => println!("{}", get_show_id(e.1.as_str())?)
+            }
+        }
+    } else if let Some(submatch) = a.subcommand_matches("getid") {
+        let id = submatch.value_of("SHOW").unwrap();
+        let id: u32 = if a.is_present("use-id") {
+            match id.parse() {
+                Ok(o) => o,
+                Err(e) => {
+                    eprintln!("[ERROR] SHOW expected positive numeric id: {}", e);
+                    exit(-2)
+                }
+            }
+        } else {
+            get_show_id(id)?
+        };
+        println!("{}", id);
+    } else {
+        panic!("subcommand error")
     }
+    Ok(())
 }
 
 #[derive(Debug)]
 enum PageError {
     PageResponseError(u16),
     IoError(std::io::Error),
-    ParseError
+    ParseError(&'static str),
+    CommandLineError(&'static str)
 }
 
 impl Display for PageError {
@@ -95,7 +139,8 @@ impl Display for PageError {
         match self {
             PageError::PageResponseError(c) => f.write_fmt(format_args!("Response Error: {}", c)),
             PageError::IoError(e) => f.write_fmt(format_args!("IO Error: {}", e)),
-            PageError::ParseError => f.write_str("Parsing Failure")
+            PageError::ParseError(s) => f.write_fmt(format_args!("Parsing Failure: {}", *s)),
+            PageError::CommandLineError(s) => f.write_fmt(format_args!("Command Line Failure: {}", *s))
         }
     }
 }
@@ -118,7 +163,15 @@ fn get_epoch() -> u64 {
     SystemTime::now().duration_since(UNIX_EPOCH).expect("back in time").as_secs()
 }
 
-fn get_show_torrents(id: u32, epoch: u64, use_batches: bool) -> Result<Vec<(String, String)>, PageError> {
+#[derive(Copy, Clone)]
+enum LinkType {
+    Res1080,
+    Res720,
+    Res480,
+    Auto
+}
+
+fn get_show_torrents(id: u32, epoch: u64, link_type: LinkType, use_batches: bool) -> Result<Vec<(String, String)>, PageError> {
     let mut idx = 0;
     let mut ls = Vec::new();
     loop {
@@ -133,7 +186,9 @@ fn get_show_torrents(id: u32, epoch: u64, use_batches: bool) -> Result<Vec<(Stri
         }
         let div_find = Selector::parse(".rls-info-container").unwrap();
         let name_find = Selector::parse(".rls-label").unwrap();
-        let link_find = Selector::parse(".link-1080p").unwrap();
+        let link_1080_find = Selector::parse(".link-1080p").unwrap();
+        let link_720_find = Selector::parse(".link-720p").unwrap();
+        let link_480_find = Selector::parse(".link-480p").unwrap();
         let link_extract = Selector::parse("a").unwrap();
         let h = Html::parse_document(data.as_str());
         for de in h.select(&div_find) {
@@ -141,9 +196,18 @@ fn get_show_torrents(id: u32, epoch: u64, use_batches: bool) -> Result<Vec<(Stri
                 let name = de
                     .select(&name_find)
                     .next()?.text().collect::<String>();
-                let link = de
-                    .select(&link_find)
-                    .next()?.select(&link_extract)
+                let link = match link_type {
+                    LinkType::Res1080 => de.select(&link_1080_find).next(),
+                    LinkType::Res720 => de.select(&link_720_find).next(),
+                    LinkType::Res480 => de.select(&link_480_find).next(),
+                    LinkType::Auto => {
+                        de.select(&link_1080_find).next()
+                            .or_else(|| de.select(&link_720_find).next())
+                            .or_else(|| de.select(&link_480_find).next())
+                    }
+                }?;
+                let link = link
+                    .select(&link_extract)
                     .next()?.value().attrs().filter_map(|v| {
                     if v.0 == "href" {
                         Some(v.1)
@@ -153,7 +217,7 @@ fn get_show_torrents(id: u32, epoch: u64, use_batches: bool) -> Result<Vec<(Stri
                 }).next()?;
                 ls.push((name, String::from(link)));
                 Some(())
-            })().ok_or(PageError::ParseError)?;
+            })().ok_or(PageError::ParseError("failed to extract magnet link"))?;
         }
     }
     Ok(ls)
@@ -165,8 +229,8 @@ lazy_static! {
 
 fn get_show_id(url: &str) -> Result<u32, PageError> {
     let data = download_page(url)?;
-    let id = ID_FINDER.captures(data.as_str()).unwrap().get(1).ok_or(PageError::ParseError)?;
-    id.as_str().parse().map_err(|_| PageError::ParseError)
+    let id = ID_FINDER.captures(data.as_str()).unwrap().get(1).ok_or(PageError::ParseError("failed to extract show id"))?;
+    id.as_str().parse().map_err(|_| PageError::ParseError("failed to parse extracted show id"))
 }
 
 lazy_static! {
